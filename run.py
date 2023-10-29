fc2575e8077efa34e3308789707080dc6c2d50ef
S Bester <sarahleebester@gmail.com>
2023-10-29 11:23:40 +0000

    feat(run): Add main menu layout

--- run.py
+++ run.py
@@ -12,3 +12,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("tidy_tasks")
 
+def main_menu():
+    """
+    Displays main menu for Tidy Tasks
+    """
+    print("Welcome to Tidy Tasks\n")
+    print("Please select an option:")
+    print("1. View To Do Lists")
+    print("2. Create a To Do List")
+    print("3. Help")
+    print("4. Exit\n")
+    user_choice = input("Enter your choice: \n")
+    print(f"You have selected {user_choice}\n")
+
+main_menu()
